278792423da6ec69a46bb92a5174f1ca8b6b45f0
Atamas Ilya <atamas1llya@gmail.com>
2017-10-16 12:22:41 +0300

    Configure pipeline

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@ import chalk from 'chalk';
 import devConfig from './envs/dev';
 import testConfig from './envs/test';
 import prodConfig from './envs/production';
+import stagingConfig from './envs/staging';
 
 const env = process.env.NODE_ENV || 'dev';
 
@@ -10,6 +11,7 @@ const config = {
   dev: devConfig,
   test: testConfig,
   production: prodConfig,
+  staging: stagingConfig,
 };
 
 console.log(chalk.cyan(`Using environment: ${env}\n`));
